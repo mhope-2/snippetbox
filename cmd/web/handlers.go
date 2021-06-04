7877890f438c8eeb546daa55980e1383a463c017
mhope <mhope@npontu.com>
2021-06-04 23:21:59 +0000

    feat: inital setup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,1 +1,54 @@
 package main
+import ( "fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+	http.NotFound(w, r)
+	return
+}
+	// Use the template.ParseFiles() function to read the template file into a
+	//template set. If there's an error, we log the detailed error message and
+	//the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles("./ui/html/home.page.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// We then use the Execute() method on the template set to write the template
+	//content as the response body. The last parameter to Execute() represents
+	//dynamic data that we want to pass in, which for now we'll leave as nil.
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+	w.Header().Set("Allow", "POST")
+	http.Error(w, "Method Not Allowed", 405)
+	return
+}
+
+	w.Write([]byte("Create a new snippet..."))
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,64 +1,12 @@
 package main
-
-import (
-	"fmt"
-	"log"
+import ( "log"
 	"net/http"
-	"strconv"
 )
-func home(w http.ResponseWriter, r *http.Request) {
-	// Check if the current request URL path exactly matches "/". If it doesn't
-	//the http.NotFound() function to send a 404 response to the client.
-	// Importantly, we then return from the handler. If we don't return the handler
-	//would keep executing and also write the "Hello from SnippetBox" message.
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
-// Add a showSnippet handler function.
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id parameter from the query string and try to
-	//convert it to an integer using the strconv.Atoi() function. If it can't
-	//be converted to an integer, or the value is less than 1, we return a 404
-	//not found response.
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	// Use the fmt.Fprintf() function to interpolate the id value with our resp
-	//and write it to the http.ResponseWriter.
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-// Add a createSnippet handler function.
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	// Use r.Method to check whether the request is using POST or not.
-	// If it's not, use the w.WriteHeader() method to send a 405 status code an
-	//the w.Write() method to write a "Method Not Allowed" response body. We
-	//then return from the function so that the subsequent code is not execute
-	if r.Method != "POST" {
-		// Use the Header().Set() method to add an 'Allow: POST' header to the
-		//response header map. The first parameter is the header name, and
-		// the second parameter is the header value.
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", 405)
-		return
-}
-
-	w.Write([]byte("Create a new snippet..."))
-}
 func main() {
-	// Register the two new handler functions and corresponding URL patterns with
-	//the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
-
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
